7ac71f8fd198b9d287b61ee10689da9d1d097465
Aito2211 <aitora1212@gmail.com>
2026-06-11 13:58:50 -0400

    Cleanup: remove newsletter files for clean build test

--- app/newsletter/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function NewsletterPage() {
-  return (
-    <div style={{padding:40}}>
-      <h1>The Stock411 Daily Brief</h1>
-      <p>Newsletter launching soon. Subscribe below to be notified.</p>
-    </div>
-  )
-}
